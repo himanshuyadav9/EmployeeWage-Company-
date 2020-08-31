325db45e6378fac5b82197fbb731ed2ef9e8848a
Himanshu <yadavhimanshu63@gmail.com>
2020-09-01 01:12:01 +0530

    till a condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,57 +1,70 @@
 public class EmployeeWage {
-       //constants
+        //constants
 	private static final int IS_FULL_TIME = 1;
 	private static final int IS_PART_TIME = 2;
 	
 	//variables
-	private int empRatePerHour;
-	private int totalDays;
+	private  int empRatePerHour;
+	private  int totalDays;
+	private  int maxHourInMonth;
 	
-	public EmployeeWage(final int empRatePerHour ,final int totalDays)
+	public EmployeeWage(final int empRatePerHour ,final int totalDays ,final int maxHourInMonth)
 	{
 		this.empRatePerHour=empRatePerHour;
 		this.totalDays=totalDays;
-	}
-	
-	//method to calculate wage
-	public void empWage() {
-		int totalEmpWage=0;
-		for ( int day=1 ; day<=totalDays ; day++)
-		{
-		int empHour = empCheck();
-		int empWage = empHour * empRatePerHour;
-		totalEmpWage=totalEmpWage+empWage;
-		System.out.println("day: "+ day + " Employee Wage: "+ empWage);
-		}
-		System.out.println( "Total Employee Wage: " + totalEmpWage);
+		this.maxHourInMonth=maxHourInMonth;
 	}
 
-	 //method return type is integer
-	public int empCheck() {
-		int empHour = 0;
-		int empCheck = (int) ((Math.random() * 10) % 3);
-		switch (empCheck)  {
-		case IS_FULL_TIME:
-			System.out.println("Employee is Present for Full Time");
-			empHour = 8;
-			break;
 
-		case IS_PART_TIME:
-			System.out.println("Employee is Present for Part Time");
-			empHour = 4;
-			break;
+// method to calculate employee wage
+    public void empWage() {
 
-		default:
-			System.out.println("Employee is Absent");
-			empHour = 0;
-		}
-		return empHour;
+	// variables
+	int totalEmpWage = 0;
+	int totalEmpHrs = 0;
+	int totalWorkingDays = 0;
 
+	// while is used for till a condition type
+	while (totalEmpHrs < maxHourInMonth && totalWorkingDays < totalDays) {
+		totalWorkingDays++;
+		final int empHour = empCheck();
+		final int empWage = empHour * empRatePerHour;
+		totalEmpHrs = totalEmpHrs + empHour;
+		totalEmpWage = totalEmpWage + empWage;
+		System.out.println("day: " + totalWorkingDays + " Employee Wage: " + empWage);
 	}
+	System.out.println("Total Employee Wage: " + totalEmpWage);
+}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-		EmployeeWage emp = new EmployeeWage(20, 20);
-		emp.empWage();
-      }
+// empCheck method to get empHour
+public int empCheck() {
+
+	int empHour = 0;
+	int empCheck = (int) ((Math.random() * 10) % 3);
+	switch (empCheck) {
+	case IS_FULL_TIME:
+		System.out.println("Employee is Present for Full Time");
+		empHour = 8;
+		break;
+
+	case IS_PART_TIME:
+		System.out.println("Employee is Present for Part Time");
+		empHour = 4;
+		break;
+
+	default:
+		System.out.println("Employee is Absent");
+		empHour = 0;
+	}
+	return empHour;
+
+}
+
+    // main method to create an object to class and call method
+    public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage Computation");
+	EmployeeWage emp = new EmployeeWage(20, 20, 100);
+	emp.empWage();
+
+    }
 }
