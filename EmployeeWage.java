8c3226b6950094a9ef966925d57cc2b7d4eb0cbb
Himanshu <yadavhimanshu63@gmail.com>
2020-09-01 01:08:47 +0530

    Wage for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,49 +1,57 @@
 public class EmployeeWage {
-   
-	       //constants
-            private static final int ISFULLTIME=1;
-	    private static final int ISPARTTIME=2;
-		
-           //variables
-	    private  int empRate;
-		
-	    public EmployeeWage(final int empRate)
-	    {
-	    	this.empRate=empRate;
-	    }
-	    
-		public void empWage() {
-			int empHour = empCheck();
-			int empWage = empHour * empRate;
-			System.out.println("Employee Wage: " + empWage);
+       //constants
+	private static final int IS_FULL_TIME = 1;
+	private static final int IS_PART_TIME = 2;
+	
+	//variables
+	private int empRatePerHour;
+	private int totalDays;
+	
+	public EmployeeWage(final int empRatePerHour ,final int totalDays)
+	{
+		this.empRatePerHour=empRatePerHour;
+		this.totalDays=totalDays;
+	}
+	
+	//method to calculate wage
+	public void empWage() {
+		int totalEmpWage=0;
+		for ( int day=1 ; day<=totalDays ; day++)
+		{
+		int empHour = empCheck();
+		int empWage = empHour * empRatePerHour;
+		totalEmpWage=totalEmpWage+empWage;
+		System.out.println("day: "+ day + " Employee Wage: "+ empWage);
 		}
+		System.out.println( "Total Employee Wage: " + totalEmpWage);
+	}
 
-		public int empCheck() {
-			int empHour = 0;
-			int empCheck = (int) ((Math.random() * 10) % 3);
-			switch (empCheck) {
-			case ISFULLTIME:
-				System.out.println("Employee is Present for Full Time");
-				empHour = 8;
-				break;
+	 //method return type is integer
+	public int empCheck() {
+		int empHour = 0;
+		int empCheck = (int) ((Math.random() * 10) % 3);
+		switch (empCheck)  {
+		case IS_FULL_TIME:
+			System.out.println("Employee is Present for Full Time");
+			empHour = 8;
+			break;
 
-			case ISPARTTIME:
-				System.out.println("Employee is Present for Part Time");
-				empHour = 4;
-				break;
+		case IS_PART_TIME:
+			System.out.println("Employee is Present for Part Time");
+			empHour = 4;
+			break;
 
-			default:
-				System.out.println("Employee is Absent");
-				empHour = 0;
-			}
-			return empHour;
-
+		default:
+			System.out.println("Employee is Absent");
+			empHour = 0;
 		}
+		return empHour;
 
-		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation");
-			EmployeeWage emp = new EmployeeWage(20);
-			emp.empWage();
+	}
 
-		}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		EmployeeWage emp = new EmployeeWage(20, 20);
+		emp.empWage();
+      }
 }
